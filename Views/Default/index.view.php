32fb7f26f9f024b87fca3b3e832408f43a552c96
Nganthoiba <Nganthoiba@DESKTOP-CLD3BN8>
2021-01-07 17:11:57 +0530

    test

--- Views/Default/index.view.php
+++ Views/Default/index.view.php
@@ -2,8 +2,12 @@
 use MyEasyPHP\Libs\Config;
 use MyEasyPHP\Libs\Html;
 Html::setContainer("_masterPage");
+
+$name = htmlspecialchars_decode($name);
+//header("Location: ".Config::get('host')."/".$name."/index");
+//redirect($name);
 ?>
 <div class="container">
     <p>Index page of Default Controller</p>
-    Hello <?= htmlspecialchars_decode($name); ?>
+    Hello <?= $name ?>
 </div>
